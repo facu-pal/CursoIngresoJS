284aed4a7cee356ef1cd9dfcef8b659b161628a0
facu-pal <facundo.palmieri.01@gmail.com>
2021-01-30 20:08:49 -0300

    segundo commit

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,10 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	let nombre;
+
+	nombre = prompt("Ingrese su nombre")  //recibe un dato un parametro y devuelve lo que el usuario puso en la caja de texto 
+	
+	alert(nombre)  //recibe un dato y no devuelve nada
 }
 
